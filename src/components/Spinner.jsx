f29f7362220cb9976c75c11003c14557ca5b851b
Joel <joelangeltw@gmail.com>
2021-10-08 20:25:17 -0500

    spinner added

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+const Spinner = () => {
+  return (
+    <div className="container">
+      <span className="loader">
+        <span className="loader-inner"></span>
+      </span>
+    </div>
+  );
+};
+
+export default Spinner;
