c2219ee4e3da83bb17b92c7dafcb03fb6ceb2bbf
Joel <joelangeltw@gmail.com>
2021-10-08 20:28:38 -0500

    movie card component added

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import Button from "./Button";
+import { Link } from "react-router-dom";
 
 const Moviecard = ({ movie }) => {
   const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
   return (
-    <li className="card">
+    <Link className="card" to={"/movies/" + movie.id}>
       <img src={imageUrl} alt="movie image" className="card__img" />
       <p className="card__title">{movie.title}</p>
       <p className="card__date">{movie.release_date}</p>
-      <Button className="button--light" content='Get tickets'/>
-    </li>
+    </Link>
   );
 };
 
