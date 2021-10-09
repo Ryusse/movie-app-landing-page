2134f6b36291407fb1d11279e0b3ea872c44561b
Joel <joelangeltw@gmail.com>
2021-10-08 20:29:02 -0500

    movie list component added

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -1,13 +1,29 @@
 import React from "react";
-import movies from "../assets/data/movies.json";
 import Moviecard from "./MovieCard";
+import { useState, useEffect } from "react";
+import { get } from "../utils/httpClient";
+import Spinner from "./Spinner";
 
 const Movieslist = () => {
-  console.log(movies);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true)
+    get("/discover/movie").then((data) => {
+      setMovies(data.results);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if(isLoading){
+    return <Spinner/>
+  }
+
   return (
-    <ul className="list">
+    <ul className="moviesList">
       {movies.map((movie) => (
-        <Moviecard key={movie.id} movie={movie}/>
+        <Moviecard key={movie.id} movie={movie} />
       ))}
     </ul>
   );
