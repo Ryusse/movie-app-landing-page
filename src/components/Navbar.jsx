f8194ec610c8ea24c7b8bf252fa1dd9886f0acd1
Joel <joelangeltw@gmail.com>
2021-10-14 22:51:05 -0500

    responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,30 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {useState} from "react";
+import { BrowserRouter as   Switch, Route ,Link } from "react-router-dom";
+import Menu from "./Menu";
+import Toggle from "./Toggle";
 
 const Navbar = () => {
+
+  const [open , setOpen ] = useState(false);
+
+  const handleToggle = () =>{
+    setOpen(!open)
+  }
+
   return (
     <nav className="navbar">
-      <li className="navbar__item">
+
+      <div className="navbar__item">
         <Link className="navbar__link" to="/">
           LOGO
         </Link>
-      </li>
+      </div>
+
+      <div className="navbar__item" onClick={handleToggle}>
+        <Toggle open = {open}/>
+      </div>
+
+        {open && <Menu />}
 
       <ul className="navbar__list">
         <li className="navbar__item">
@@ -28,10 +44,6 @@ const Navbar = () => {
         </li>
         <li className="navbar__item">
           My list <i className="fas fa-chevron-down"></i>
-        </li>
-
-        <li className="navbar__item">
-          <i className="fas fa-search"></i>
         </li>
       </ul>
     </nav>
