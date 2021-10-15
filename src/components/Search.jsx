63b1d03f314fb70a0c2b3c7c479ec7955ac7086e
Joel <joelangeltw@gmail.com>
2021-10-14 22:50:03 -0500

    form created , input status management

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,37 @@
+import { useState, useEffect} from "react";
+import { useHistory } from "react-router";
+import { useQuery } from "../hooks/useQuery";
+
 export default function Search() {
-    return (
-        <div>
-            <form>
-                <input type="text" />
-                <button type="submit">Search</button>
-            </form>
-        </div>
-    )
+  const [searchText, setSearchText] = useState("");
+  //Añadir elemento a la ruta
+  const history = useHistory();
+  const query = useQuery();
+  const search = query.get("search");
+
+  useEffect(() => {
+    setSearchText(search || "");
+  }, [search]);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    history.push("?search=" + searchText);
+  };
+
+  return (
+    <form className="search" onSubmit={handleSubmit}>
+      <div className="search__container">
+        <input
+          className="search__input"
+          type="text"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button className="search__button" type="submit">
+          Search
+        </button>
+      </div>
+    </form>
+  );
 }
