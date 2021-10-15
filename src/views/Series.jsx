7dea57297412959edc9f6372f434788a0877fc42
Joel <joelangeltw@gmail.com>
2021-10-14 22:44:18 -0500

    navbar added on the page series

--- src/views/Series.jsx
+++ src/views/Series.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 const Series = () => {
     return (
-        <div>
-            series
-        </div>
+        <React.Fragment>
+            <Navbar/>
+            
+        </React.Fragment>
     );
 }
 
