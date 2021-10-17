cc0f26b5ea19a5f1df2ac51cad9ee477443288f0
Joel <joelangeltw@gmail.com>
2021-10-17 17:09:27 -0500

    multiple useState, function handdleToggleSearch created , search and searchContainer, added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,51 +1,46 @@
-import {useState} from "react";
-import { BrowserRouter as   Switch, Route ,Link } from "react-router-dom";
+import { useState } from "react";
+import { BrowserRouter as Switch, Route, Link } from "react-router-dom";
 import Menu from "./Menu";
 import Toggle from "./Toggle";
+import Button from "./Button";
+import Search from "./Search";
+import SearchContainer from "./SearchContainer";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [searchOpen, setSearchOpen] = useState(false);
 
-  const [open , setOpen ] = useState(false);
+  const handleToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-  const handleToggle = () =>{
-    setOpen(!open)
+  const handleToggleSearch = () => {
+    setSearchOpen(!searchOpen);
   }
 
   return (
     <nav className="navbar">
+      <div className="navbar__item" onClick={handleToggle}>
+        <Toggle open={menuOpen} />
+      </div>
+      <Menu open={menuOpen} />
 
       <div className="navbar__item">
         <Link className="navbar__link" to="/">
-          LOGO
+          <div className="logo">LOGO</div>
         </Link>
       </div>
 
-      <div className="navbar__item" onClick={handleToggle}>
-        <Toggle open = {open}/>
+      <div className="navbar__item" onClick={handleToggleSearch}>
+        <Search />
       </div>
 
-        {open && <Menu />}
+      <SearchContainer open={searchOpen}/>
 
-      <ul className="navbar__list">
-        <li className="navbar__item">
-          <Link className="navbar__link" to="/">
-            Home
-          </Link>
-        </li>
-        <li className="navbar__item">
-          <Link className="navbar__link" to="/movies">
-            Movies
-          </Link>
-        </li>
-        <li className="navbar__item">
-          <Link className="navbar__link" to="/series">
-            Series
-          </Link>
-        </li>
-        <li className="navbar__item">
-          My list <i className="fas fa-chevron-down"></i>
-        </li>
-      </ul>
+      <div className="navbar__item">
+        <Button className="button--sign-up" content="Sign up" />
+      </div>
+
     </nav>
   );
 };
