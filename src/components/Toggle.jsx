f1d2203af1a64ffb3ce93f7f953001a164eebc24
Joel <joelangeltw@gmail.com>
2021-10-17 17:05:41 -0500

    line class removed

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function toggle(props) {
     return (
         <div className={`toggle ${props.open ? "open" : ""}`} >
-            <div className={`toggle__line toggle__line-1 ${props.open ? "open" : ""}`}></div>
-            <div className={`toggle__line toggle__line-2 ${props.open ? "open" : ""}`}></div>
-            <div className={`toggle__line toggle__line-3 ${props.open ? "open" : ""}`}></div>
+            <div className={` ${props.open ? "open" : ""}`}></div>
+            <div className={` ${props.open ? "open" : ""}`}></div>
+            <div className={` ${props.open ? "open" : ""}`}></div>
         </div>
     )
 }
