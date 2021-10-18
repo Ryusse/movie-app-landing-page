7e1d711b5b482b728b4c27a98f03e191d649b44b
Joel <joelangeltw@gmail.com>
2021-10-18 15:08:30 -0500

    exit button

---
+++ src/components/ExitButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ExitButton(props) {
+    return (
+        <div className={`${props.className}`}>
+          <div></div>
+          <div></div>
+        </div>
+    )
+}
