ae4e7413fdfbb088e6f156df762a29c4d4c94970
Joel <joelangeltw@gmail.com>
2021-10-21 14:39:03 -0500

    props close

--- src/components/ExitButton.jsx
+++ src/components/ExitButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ExitButton(props) {
     return (
-        <div className={`${props.className}`}>
+        <div className={`${props.className}`} onClick={props.close}>
           <div></div>
           <div></div>
         </div>
