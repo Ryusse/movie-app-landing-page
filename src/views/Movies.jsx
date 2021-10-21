343ce81705e235a82c407b311851c4776459b1c2
Joel <joelangeltw@gmail.com>
2021-10-21 14:40:06 -0500

    space removed

--- src/views/Movies.jsx
+++ src/views/Movies.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import Hero from "../components/Hero";
-import MoviesList from "../components/MoviesList";
-import Navbar from "../components/Navbar";
-import Search from "../components/Search";
+import React from 'react';
+import Hero from '../components/Hero';
+import MoviesList from '../components/MoviesList';
+import Navbar from '../components/Navbar';
+import Search from '../components/Search';
 
 const Movies = () => {
   return (
     <React.Fragment>
-      <Navbar/>
+      <Navbar />
       <section className="content">
         <h2>All Movies</h2>
         <MoviesList />
