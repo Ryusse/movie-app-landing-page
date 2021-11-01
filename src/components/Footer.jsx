4f0f8c2f48c358333f96207bf554bd8a8b44b520
Joel <joelangeltw@gmail.com>
2021-11-01 18:10:21 -0500

    small font size class added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,45 +17,45 @@ export default function Footer() {
       <div className='footer__container'>
         <div className='footer__row'>
           <h4 className='footer__header'>Resources</h4>
-          <p tabIndex='6' className='footer__link'>
+          <p tabIndex='6' className='footer__link fs-small'>
             Contact Us
           </p>
-          <p tabIndex='6' className='footer__link'>
+          <p tabIndex='6' className='footer__link fs-small'>
             Forums
           </p>
-          <p tabIndex='6' className='footer__link'>
+          <p tabIndex='6' className='footer__link fs-small'>
             Blog
           </p>
-          <p tabIndex='6' className='footer__link'>
+          <p tabIndex='6' className='footer__link fs-small'>
             Help Center
           </p>
         </div>
 
         <div className='footer__row'>
           <h4 className='footer__header'>Account</h4>
-          <p tabIndex='6' className='footer__link'>
+          <p tabIndex='6' className='footer__link fs-small'>
             My Account
           </p>
-          <p tabIndex='6' className='footer__link'>
+          <p tabIndex='6' className='footer__link fs-small'>
             Watchlist
           </p>
-          <p tabIndex='6' className='footer__link'>
+          <p tabIndex='6' className='footer__link fs-small'>
             Collections
           </p>
-          <p tabIndex='6' className='footer__link'>
+          <p tabIndex='6' className='footer__link fs-small'>
             User Guide
           </p>
         </div>
 
         <div className='footer__row'>
           <h4 className='footer__header'>Legal</h4>
-          <p tabIndex='6' className='footer__link'>
+          <p tabIndex='6' className='footer__link fs-small'>
             Terms of Use
           </p>
-          <p tabIndex='6' className='footer__link'>
+          <p tabIndex='6' className='footer__link fs-small'>
             Privacy Policy
           </p>
-          <p tabIndex='6' className='footer__link'>
+          <p tabIndex='6' className='footer__link fs-small'>
             Security
           </p>
         </div>
